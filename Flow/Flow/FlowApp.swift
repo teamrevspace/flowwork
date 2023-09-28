b80a56f83520263aaadf6cb0b7117646f315cda0
Allen Lin <allenlinsh@gmail.com>
2023-09-28 14:26:22 -0400

    init swiftui

---
+++ Flow/Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Allen Lin on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
