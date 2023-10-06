95387ae38496a9ffcec9741c86fad870d2c1e865
Allen Lin <allenlinsh@gmail.com>
2023-10-06 13:33:23 -0400

    switch from gigalixir to fly deployment

--- flow_stream/config/prod.exs
+++ flow_stream/config/prod.exs
@@ -14,6 +14,6 @@ config :logger, level: :info
 config :flow_stream, FlowStreamWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   http: [port: {:system, "PORT"}],
-  url: [host: "#{System.get_env("APP_NAME")}.gigalixirapp.com", port: 443],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  url: [host: "#{System.get_env("APP_NAME")}.fly.dev", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true
