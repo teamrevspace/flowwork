6873a3e2eac80589a1e411d9770bb5a575f56719
Allen Lin <allenlinsh@gmail.com>
2023-10-05 23:28:46 -0400

    Set prod config for elixir

--- flow_stream/config/prod.exs
+++ flow_stream/config/prod.exs
@@ -11,3 +11,9 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+config :flow_stream, FlowStreamWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
