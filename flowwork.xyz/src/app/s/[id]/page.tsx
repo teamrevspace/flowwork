638b080775edb26ce8333beabce23b4d437a25ae
Allen Lin <allenlinsh@gmail.com>
2023-10-14 01:39:24 -0400

    fix build error

--- flowwork.xyz/src/app/s/[id]/page.tsx
+++ flowwork.xyz/src/app/s/[id]/page.tsx
@@ -1,14 +1,9 @@
-// https://flowwork.xyz/s/LfLR9zF9lmvMSmzy1bPU
 "use client"
 
 import Link from "next/link";
 import { useEffect } from "react";
 
 export default function JoinSessionPage({ params }: { params: { id: string } }) {
-  useEffect(() => {
-    joinSession()
-  }, [])
-
   const downloadApp = async () => {
     window.location.href = 'https://flowwork.xyz/download/Flow_Work.dmg'
   }
@@ -17,6 +12,10 @@ export default function JoinSessionPage({ params }: { params: { id: string } }) 
     window.location.href = `flowwork://join?sessionId=${params.id}`
   }
 
+  useEffect(() => {
+    joinSession()
+  }, [ joinSession ])
+
   return (
     <div className="h-screen w-full flex flex-col bg-white px-6">
       <div className="flex flex-col items-center justify-center flex-1 gap-y-4">
@@ -24,7 +23,7 @@ export default function JoinSessionPage({ params }: { params: { id: string } }) 
           <img className="h-24 w-24 sm:h-32 sm:w-32" src="/logo.png" />
         </Link>
         <h1 className="text-2xl sm:text-4xl font-bold text-center text-[#001122]">Launching Flow Work 🚀</h1>
-        <p className="text-base sm:text-lg text-center text-[#999999]">If you don't have Flow Work installed,&nbsp;<button onClick={(e) => {
+        <p className="text-base sm:text-lg text-center text-[#999999]">If you don&apos;t have Flow Work installed,&nbsp;<button onClick={(e) => {
           e.preventDefault()
           downloadApp()
         }} className="text-electric-blue">{`click here`}</button></p>
@@ -34,7 +33,7 @@ export default function JoinSessionPage({ params }: { params: { id: string } }) 
         }} type="button" className="px-8 sm:px-12 py-2 sm:py-3 border-none rounded-xl text-base sm:text-lg font-medium bg-electric-blue hover:bg-electric-blue-accent text-white">
           Join session
         </button>
-        <p className="text-sm sm:text-base text-[#999999] text-center">or copy-paste the session code: <code className="bg-silver">{params.id}</code></p>
+        <p className="text-sm sm:text-base text-[#999999] text-center">or copy-paste the session code:&nbsp;<code className="bg-silver">{params.id}</code></p>
       </div>
     </div>
   )
