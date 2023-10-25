697302ada84d7f107f5bcd9f25d63abb2fcaa93f
Allen Lin <allenlinsh@gmail.com>
2023-10-25 15:10:01 -0400

    add session name on invite page

--- flowwork.xyz/src/app/s/[id]/page.tsx
+++ flowwork.xyz/src/app/s/[id]/page.tsx
@@ -2,9 +2,10 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function JoinSessionPage({ params }: { params: { id: string } }) {
+  const [ sessionName, setSessionName ] = useState<string | null | undefined>(undefined);
   const router = useRouter();
 
   const downloadApp = async () => {
@@ -13,7 +14,28 @@ export default function JoinSessionPage({ params }: { params: { id: string } }) 
 
   const joinSession = useCallback(async () => {
     router.push(`flowwork://join?sessionId=${params.id}`)
-  }, [params.id, router])
+  }, [ params.id, router ])
+
+  useEffect(() => {
+    fetch(`https://api.flowwork.xyz/api/sessions?session_id=${params.id}`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network failed ' + res.statusText);
+        }
+        return res.json();
+      })
+      .then(res => {
+        if (res.status === 'success') {
+          setSessionName(res.data.name)
+        } else {
+          setSessionName(null)
+        }
+      })
+      .catch(error => {
+        setSessionName(undefined)
+        console.error('API error:', error);
+      });
+  }, [ joinSession, params.id, router ])
 
   return (
     <div className="h-screen w-full flex flex-col bg-white px-6">
@@ -21,18 +43,19 @@ export default function JoinSessionPage({ params }: { params: { id: string } }) 
         <Link href="/">
           <img className="h-24 w-24 sm:h-32 sm:w-32" src="/logo.png" />
         </Link>
-        <h1 className="text-2xl sm:text-4xl font-bold text-center text-[#001122]">Launching Flow Work 🚀</h1>
-        <p className="text-base sm:text-lg text-center text-[#999999]">If you don&apos;t have Flow Work installed,&nbsp;<button onClick={(e) => {
-          e.preventDefault()
-          downloadApp()
-        }} className="text-electric-blue">{`click here`}</button></p>
-        <button onClick={(e) => {
+        <h1 className="text-2xl sm:text-4xl font-bold text-center text-[#001122]">{sessionName ? `Join ${sessionName} 🚀` : sessionName === null ? `Session not found` : `Loading...`}</h1>
+        {sessionName ? <p className="text-base sm:text-lg text-center text-[#999999]">or copy-paste the session code:&nbsp;<code className="bg-silver">{params.id}</code></p> : sessionName === null ?
+          <p className="text-base sm:text-lg text-center text-[#999999]">Either this session doesn&apos;t exist or it has been removed.</p> : null}
+        {sessionName && <button onClick={(e) => {
           e.preventDefault()
           joinSession()
         }} type="button" className="px-8 sm:px-12 py-2 sm:py-3 border-none rounded-xl text-base sm:text-lg font-medium bg-electric-blue hover:bg-electric-blue-accent text-white">
           Join session
-        </button>
-        <p className="text-sm sm:text-base text-[#999999] text-center">or copy-paste the session code:&nbsp;<code className="bg-silver">{params.id}</code></p>
+        </button>}
+        <p className="text-sm sm:text-base text-[#999999] text-center">If you don&apos;t have Flow Work installed,&nbsp;<button onClick={(e) => {
+          e.preventDefault()
+          downloadApp()
+        }} className="text-electric-blue">{`click here`}</button></p>
       </div>
     </div>
   )
